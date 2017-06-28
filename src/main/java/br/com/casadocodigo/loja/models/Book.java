74948eb92d13b5f6258d039ddd40da1878897c89
Gilvan Almeida <g6almeida@hotmail.com>
2017-06-28 14:56:33 -0300

    Mapeando a entidade.

--- src/main/java/br/com/casadocodigo/loja/models/Book.java
+++ src/main/java/br/com/casadocodigo/loja/models/Book.java
@@ -2,8 +2,15 @@ package br.com.casadocodigo.loja.models;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
 	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 	private String description;
