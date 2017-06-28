efe29126dca0930c77d9904cf7abbcceb0f08920
Gilvan Almeida <g6almeida@hotmail.com>
2017-06-27 23:05:30 -0300

    Instânciando o objeto e acesso aos atributos da classe 'Book'.

--- src/main/java/br/com/casadocodigo/loja/managedbeans/AdminBooksBean.java
+++ src/main/java/br/com/casadocodigo/loja/managedbeans/AdminBooksBean.java
@@ -2,11 +2,18 @@ package br.com.casadocodigo.loja.managedbeans;
 
 import javax.enterprise.inject.Model;
 
+import br.com.casadocodigo.loja.models.Book;
+
 @Model
 public class AdminBooksBean {
 	
+	private Book product = new Book();
+	
 	public void save(){
-		System.out.println("Precisamos salvar livros!!");
+		System.out.println(product);
 	}
-
+	
+	public Book getProduct() {
+		return product;
+	}
 }
