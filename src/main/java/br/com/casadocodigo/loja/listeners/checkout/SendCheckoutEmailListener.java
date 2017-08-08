6306d9eb9bfd00852a7377b9c17c0d323dfef871
Gilvan Almeida <g6almeida@hotmail.com>
2017-08-08 16:12:39 -0300

    [Jms] Enviando email.

--- src/main/java/br/com/casadocodigo/loja/listeners/checkout/SendCheckoutEmailListener.java
+++ src/main/java/br/com/casadocodigo/loja/listeners/checkout/SendCheckoutEmailListener.java
@@ -1,14 +1,23 @@
 package br.com.casadocodigo.loja.listeners.checkout;
 
+import javax.annotation.Resource;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
-import javax.jms.JMSException;
+import javax.inject.Inject;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
+import javax.mail.Message.RecipientType;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import br.com.casadocodigo.loja.daos.CheckoutDAO;
+import br.com.casadocodigo.loja.models.Checkout;
 
 @MessageDriven(activationConfig = {
 		@ActivationConfigProperty(
@@ -19,13 +28,27 @@ public class SendCheckoutEmailListener implements MessageListener{
 	
 	private Logger logger = LoggerFactory.getLogger(SendCheckoutEmailListener.class);
 	
+	@Resource(mappedName = "java:jboss/mail/gmail")
+	private Session session;
+	
+	@Inject
+	private CheckoutDAO checkouDAO;
+	
 	@Override
 	public void onMessage(Message message){
 		TextMessage text = (TextMessage) message;
 		
 		try {
-			System.out.println("Enviando email " +text.getText());
-		} catch (JMSException e) {
+			Checkout checkout = checkouDAO.findByUuid(text.getText());
+			
+			javax.mail.Message mimeMessage = new MimeMessage(session);
+			
+			mimeMessage.setRecipients(RecipientType.TO, InternetAddress.parse("g6negao@gmail.com"));
+			mimeMessage.setSubject("Sua compra foi registrada");
+			mimeMessage.setContent("<html><body>Compra realizada com sucesso. O código de acompanhamento é "+checkout.getUuid()+"</body></html>","text/html");
+			
+			Transport.send(mimeMessage);
+		} catch (Exception e) {
 			// TODO: handle exception
 			logger.error("Problema no envio do email",e);
 		}
