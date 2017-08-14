d65622dbf0523ef3e72bf801185e60c37dbd4982
Gilvan Almeida <g6almeida@hotmail.com>
2017-08-14 14:49:15 -0300

    [seguranca] Protegendo os trechos da pagina.

---
+++ src/main/java/br/com/casadocodigo/loja/security/CurrentUser.java
@@ -0,0 +1,17 @@
+package br.com.casadocodigo.loja.security;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+
+@Model
+public class CurrentUser {
+	
+	@Inject
+	private HttpServletRequest request;
+	
+	public boolean hasRole(String name){
+		return request.isUserInRole(name);
+	}
+
+}
