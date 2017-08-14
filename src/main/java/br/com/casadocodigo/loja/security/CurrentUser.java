9ad2ef57c730e80fa62c3b90ed992b896b616d02
Gilvan Almeida <g6almeida@hotmail.com>
2017-08-14 16:14:46 -0300

    [seguranca] carregando todas as informacoes do usuario.

--- src/main/java/br/com/casadocodigo/loja/security/CurrentUser.java
+++ src/main/java/br/com/casadocodigo/loja/security/CurrentUser.java
@@ -1,8 +1,11 @@
 package br.com.casadocodigo.loja.security;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+
+import br.com.casadocodigo.loja.models.SystemUser;
 
 @Model
 public class CurrentUser {
@@ -10,12 +13,27 @@ public class CurrentUser {
 	@Inject
 	private HttpServletRequest request;
 	
+	@Inject
+	private SecurityDAO securityDAO;
+	private SystemUser systemUser;
+		
 	public boolean hasRole(String name){
 		return request.isUserInRole(name);
 	}
 	
 	public String getLogin(){
 		return request.getUserPrincipal().getName();
+	}
+	
+	public SystemUser get(){
+		return this.systemUser;
+	}
+	
+	@PostConstruct
+	private void loadSystemUser(){
+		if (request.getUserPrincipal() != null) {
+			this.systemUser = securityDAO.loadUserByUsername(request.getUserPrincipal().getName());
+		}
 	}
 
 }
